5b7456cd606d47505a6302b48059679819fd3c52
Vladyslav <vladkovach00@gmail.com>
2025-02-09 09:17:41 +0100

    Auto-generated commit

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -6,8 +6,8 @@ export default {
   locale: undefined,
   routes: undefined,
   assets: {
-    'index.csr.html': {size: 957, hash: '02af45e903d23000680b97d8cc497b2943ea2c8d2df6f8cfe98aae81c66c6cb0', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1053, hash: 'ae12bc585de7a77ebe90c1d17c45d36c6f88d5f7c8ac72051d60114a2aa36606', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 957, hash: 'c4832eff5b7891b874b01bfc4ac6274e4d6006950d0c3ee29019fa6f0a6e88e9', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1053, hash: '4261f82c1b798d95cf36b9953644f87df46199b537fe041a5bf8fa0c72eb2a00', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-UQMYCOXW.css': {size: 1272, hash: 'nfv7KolCC5U', text: () => import('./assets-chunks/styles-UQMYCOXW_css.mjs').then(m => m.default)}
   },
 };
